153cac8b061f9597736f8f923480e5061ce8037a
Talgat06 <talgatino06@gmail.com>
2024-12-01 15:36:07 +0600

    first commit

---
+++ lesson1/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import MainPage from "./pages/mainPage/MainPage";
+import AboutPage from "./pages/aboutPage/AboutPage";
+import ChangeTitle from "./components/ChangeTitle";
+import ContactsPage from "./pages/contactsPage/ContactsPage";
+
+function App() {
+  return (
+    <div className="App">
+        {/*<MainPage/>*/}
+        {/*<h1>------------------</h1>*/}
+        {/*<AboutPage/>*/}
+        {/*<ChangeTitle/>*/}
+        <ContactsPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ lesson1/src/pages/aboutPage/AboutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {useSelector} from "react-redux";
+
+const AboutPage = () => {
+
+    const aboutTitle = useSelector(state => state.aboutTitle)
+
+    return (
+        <h1>{aboutTitle}</h1>
+    );
+};
+
+export default AboutPage;
