60dfba6be653f0f4dc8817ff2d79a587781b3521
Talgat06 <talgatino06@gmail.com>
2024-12-01 20:07:58 +0600

    first commit

--- homework1/src/counter/Counter.js
+++ homework1/src/counter/Counter.js
@@ -13,9 +13,11 @@ const Counter = () => {
     return (
         <div style={{ textAlign: 'center'}}>
             <div>
-                <button onClick={() => decrement(1)}>-</button>
+                <button onClick={() => decrement(1)}>-1</button>
+                <button onClick={() => decrement(10)}>-10</button>
                 {count}
-                <button onClick={() => increment(1)}>+</button>
+                <button onClick={() => increment(1)}>+1</button>
+                <button onClick={() => increment(10)}>+10</button>
                 <button onClick={reset}>RESET</button>
             </div>
         </div>
